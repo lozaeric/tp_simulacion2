3070f8bcd479eb5bb9f1739eee78eca1dc63bf14
Eric Loza <lozaeric@gmail.com>
2016-04-19 14:19:23 -0300

    ID agregado a profesor

--- src/Profesor.java
+++ src/Profesor.java
@@ -3,11 +3,15 @@ public class Profesor {
 	private String[] materias;
 	private Horario[] horarios;
 	private String nombre;
+	private byte id;
+	private static byte _id = 0;
 	
 	public Profesor(String[] materias, Horario[] horarios, String nombre) {
 		this.materias = materias;
 		this.horarios = horarios;
 		this.nombre = nombre;
+		_id += 1;
+		id = _id;
 	}
 	public String[] getMaterias() {
 		return materias;
@@ -17,5 +21,9 @@ public class Profesor {
 	}
 	public String getNombre() {
 		return nombre;
+	}
+	
+	public byte getId () {
+		return id;
 	}
 }
